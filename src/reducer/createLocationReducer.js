af33707319d56d4e5afeb67c6618a78aaf03eca2
James Gillmore <james@faceyspacey.com>
2017-03-10 13:01:07 -0800

    test breaking new travis script

--- src/reducer/createLocationReducer.js
+++ src/reducer/createLocationReducer.js
@@ -9,7 +9,7 @@ export default (
   routesMap: RoutesMap,
 ) => (
   state: LocationState = initialState,
-  action: Action,
+  action: LocationState,
 ): LocationState => {
   if (action.type === NOT_FOUND || (routesMap[action.type]
       && (action.meta.location.current.pathname !== state.pathname || action.meta.location.load))) {
