d7a0e3e53f0a5e4106bc7e94c76fc31cc58ae0e3
Zack Jackson <zackary.l.jackson@gmail.com>
2018-05-13 17:50:56 -0400

    Adding TODO: and $flowFixMe
    Attempting to keep it readability while maintaining a 86 line length limit

--- src/createScene/utils/formatRoute.js
+++ src/createScene/utils/formatRoute.js
@@ -9,6 +9,8 @@ export default (r: RouteInput, type: string, routes: RoutesMap, formatter: ?Func
   // set default path for NOT_FOUND actions if necessary
   if (!route.path && isNotFound(type)) {
     route.path = route.scene
+      // TODO: find out where this needs to go / implement it into a type (Route?)
+      // $FlowFixMe
       ? `/${r.scene.toLowerCase()}/not-found`
       : '/not-found'
   }
