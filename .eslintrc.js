62135f5497de3c6874001e1d1e12394cbb13697a
Daniel Playfair Cal <daniel.playfair.cal@gmail.com>
2018-08-20 23:35:17 +1000

    Eslint: allow nested ternary expressions and ++ operators in for loops

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,5 +11,7 @@ module.exports = {
     'prettier/prettier': 'warn',
     'no-use-before-define': ['error', { functions: false, classes: false, variables: false }],
     'no-underscore-dangle': 0,
+    'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
+    'no-nested-ternary': 0,
   },
 }
