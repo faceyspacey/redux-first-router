00066ca61d3e5485226c685d26ce149a75fe7fd7
Sergio Toro <yiti007@gmail.com>
2018-09-13 16:23:46 +0200

    revert modifying if closing

--- src/reducer/createLocationReducer.js
+++ src/reducer/createLocationReducer.js
@@ -24,8 +24,7 @@ export default (initialState: LocationState, routesMap: RoutesMap) => (
       (typeof route === 'string' || route.path) &&
       (action.meta.location.current.pathname !== state.pathname ||
         action.meta.location.current.search !== state.search ||
-        action.meta.location.kind === 'load'
-      ))
+        action.meta.location.kind === 'load'))
   ) {
     const query = action.meta.location.current.query
     const search = action.meta.location.current.search
