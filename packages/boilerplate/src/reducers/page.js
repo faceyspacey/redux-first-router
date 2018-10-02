07464d323a7a09260c37babc8f755636db2af322
zackjackson <zackary.l.jackson@gmail.com>
2018-10-02 12:13:41 -0700

    work in progress on router

--- packages/boilerplate/src/reducers/page.js
+++ packages/boilerplate/src/reducers/page.js
@@ -1,9 +1,16 @@
-export default (state = 'HOME', action = {}) => components[action.type] || state
-
 const components = {
   HOME: 'Home',
   LIST: 'List',
   NOT_FOUND: 'NotFound',
+}
+
+export default (state = 'HOME', action = {}) => {
+  if (action.type === '@@rudy/ADD_ROUTES' && action.payload.routes) {
+    Object.assign({ ...components, ...action.payload.routes })
+    const [type] = Object.keys(action.payload.routes)
+    return type
+  }
+  return components[action.type] || state
 }
 
 // NOTES: this is the primary reducer demonstrating how RFR replaces the need
---
+++ packages/boilerplate/src/router/index.js
@@ -0,0 +1,54 @@
+/* eslint-disable */
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import * as Rudy from '@respond-framework/rudy'
+
+const routesMap = {}
+
+class Router extends Component {
+  render() {
+    const childs = Array.isArray(this.props.children) ? this.props.children : [this.props.children]
+    const routeKey = childs[0].props.component.chunkName()
+    const routes = {
+      [routeKey]: {
+        ...childs[0].props,
+      },
+    }
+
+
+    const formatRoute = (route) => ({
+      ...route,
+    })
+
+
+    this.props.addRoutes(routes, formatRoute)
+    // this.props.setRoute({ type: routeKey })
+    return <span>route</span>
+  }
+}
+
+class Route extends Component {
+  render() {
+    return null
+  }
+}
+
+const getRoutesMap = () => routesMap
+
+
+const mapDispatchToProps = (dispatch) => ({
+  addRoutes: (routes, formatRoute = null) => dispatch(Rudy.addRoutes(routes)),
+  setRoute: (object) => dispatch(object)
+})
+
+const mapState = (state) => {
+  return {}
+}
+
+const ConnectedRouter = connect(mapState, mapDispatchToProps)(Router)
+
+export {
+  Route,
+  ConnectedRouter,
+  getRoutesMap
+}
