74a61ef1e6db010ccc2620b3df9b8e2b87e40b54
Martin Törnqvist <martin.toernqvist@gmail.com>
2018-10-20 17:56:08 +0200

    Add source for examples used in docs, fix errors

---
+++ examples/docs/minimal/src/configureStore.js
@@ -0,0 +1,21 @@
+import { applyMiddleware, combineReducers, compose, createStore } from 'redux'
+import { connectRoutes } from 'redux-first-router'
+
+import page from './pageReducer'
+
+const routesMap = {
+  HOME: '/',
+  USER: '/user/:id'
+}
+
+export default function configureStore(preloadedState) {
+  const { reducer, middleware, enhancer, thunk } = connectRoutes(routesMap)
+
+  const rootReducer = combineReducers({ page, location: reducer })
+  const middlewares = applyMiddleware(middleware)
+  const enhancers = compose(enhancer, middlewares)
+
+  const store = createStore(rootReducer, preloadedState, enhancers)
+
+  return { store, thunk }
+}
